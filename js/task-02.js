a5f347809003b6efedc89eece98cd98e4150ce00
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2024-10-25 16:27:14 +0300

    564654

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = d;
+
+const markUp = ingredients.forEach((ingredient) => {
+  const listItemEL = document.createElement("li");
+  listNameEl.textContent = ingredient;
+  listItemEL.classList.add("item");
+});
